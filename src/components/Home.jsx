de8365f983c4ff1cfa520f8cbefa908c70d4de66
batariloa <batariloa@icloud.com>
2023-03-08 21:52:45 +0100

    Font size, padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,9 +19,8 @@ export function Home() {
           building efficient web solutions. With experience in both backend and
           frontend development, I'm passionate about delivering innovative
           solutions and take pride in delivering high-quality work. If you are
-          looking for a developer to help bring your web project to life, I
-          would be thrilled to have the opportunity to discuss your needs with
-          you. Please take a look around and don't hesitate to reach out.
+          looking for a developer to help bring your web project to life, please
+          don't hesitate to reach out.
         </p>
         <div>
           <Link to="work" offset={50} smooth={true} duration={600}>
